9843baeb5e11469106103bcca124f0470efe3947
reiul <ulf.reiman@arbetsformedlingen.se>
2019-06-25 10:58:14 +0200

    fix

--- src/test/java/se/jobtech/taxonomy/client/api/PublicApiTest.java
+++ src/test/java/se/jobtech/taxonomy/client/api/PublicApiTest.java
@@ -109,7 +109,7 @@ public class PublicApiTest {
     }
 
 
-    //  @Test
+    @Test
     public void v0TaxonomyPublicSearchLoadTest() {
         for (int i = 0; i < 50; i++) {
             char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
